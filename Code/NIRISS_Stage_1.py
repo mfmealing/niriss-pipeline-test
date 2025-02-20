8aadd3998fd13b246444563ddf68c6b5256bf9f8
Megan Mealing <mealingm@cardiff.ac.uk>
2025-02-20 11:34:10 +0000

    stage 1 updates

--- Code/NIRISS_Stage_1.py
+++ Code/NIRISS_Stage_1.py
@@ -76,28 +76,41 @@ for i in range(1,5):
         group = result.data[:,k,:,:]
         group_med = np.nanmedian(group, axis=0)
         group_3d = np.expand_dims(group_med, axis=0)
-        flux = np.repeat(group_3d, result.shape[0], axis=0)
+        flux = np.repeat(group_3d, result.data.shape[0], axis=0)
         group_flux.append(flux)
     
     flux_final = np.stack(group_flux, axis=1)
     f_noise = result.data - flux_final
+    plt.figure('1/f noise')
+    plt.imshow(f_noise[0][3], aspect='auto', vmax=50)
+
     
-    for j in range(f_noise.shape[0]):
-        first_pix = f_noise[j,:,0:5,:]
-        last_pix = f_noise[j,:,-5:,:]
-        f_noise_stack = np.hstack((first_pix,last_pix))
-        f_noise_med = np.nanmedian(f_noise_stack, axis=1)
-        f_noise_3d = np.expand_dims(f_noise_med, axis=1)
-        f_noise_final = np.repeat(f_noise_3d, result.shape[2], axis=1)
-        # result.data[j] = result.data[j] - f_noise_final
-        f_noise[j] = f_noise[j] - f_noise_final
-        
     # for j in range(f_noise.shape[0]):
-    #     for k in range(f_noise.shape[1]):
-    #         for l in range(f_noise.shape[3]):
-    #             col = f_noise[j,k,:,l]
-    #             col_med = np.nanmedian(col)
-    #             result.data[j,k,:,l] = result.data[j,k,:,l] - col_med 
+    #     first_pix = f_noise[j,:,0:5,:]
+    #     last_pix = f_noise[j,:,-5:,:]
+    #     f_noise_stack = np.hstack((first_pix,last_pix))
+    #     f_noise_med = np.nanmedian(f_noise_stack, axis=1)
+    #     f_noise_3d = np.expand_dims(f_noise_med, axis=1)
+    #     f_noise_final = np.repeat(f_noise_3d, result.shape[2], axis=1)
+        # f_noise[j] = f_noise[j] - f_noise_final
+    
+    first_pix = f_noise[:,:,0:5,:]
+    last_pix = f_noise[:,:,-5:,:]
+    f_noise_stack = np.dstack((first_pix,last_pix))
+    f_noise_med = np.nanmedian(f_noise_stack, axis=2)
+    f_noise_3d = np.expand_dims(f_noise_med, axis=2)
+    f_noise_final = np.repeat(f_noise_3d, result.shape[2], axis=2)
+    f_noise = f_noise - f_noise_final
+    
+    plt.figure('1/f noise final')
+    plt.imshow(f_noise[0][3], aspect='auto', vmax=50)
+    
+    xx
+
+    col_med = np.nanmedian(f_noise, axis=2)
+    med_4d = np.expand_dims(col_med, axis=2)
+    med_final = np.repeat(med_4d, result.shape[2], axis=2)
+    result.data = result.data - med_final
 
     step = LinearityStep()
     result = step.run(result)
