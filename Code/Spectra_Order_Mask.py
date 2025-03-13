a67c8c35f07f9cb36e011d63c89557d75bc79a41
Megan Mealing <mealingm@cardiff.ac.uk>
2025-03-13 12:34:22 +0000

    Changes to spectra mask

--- Code/Spectra_Order_Mask.py
+++ Code/Spectra_Order_Mask.py
@@ -82,72 +82,146 @@ low_nans = np.array(np.where((nans_frac>0) & (nans_frac<0.1)))
 result.data[:,low_nans[0],low_nans[1]] = np.apply_along_axis(interpolate_nans, axis=0, arr=result.data[:,low_nans[0],low_nans[1]])
 result.data = np.apply_along_axis(interpolate_nans, axis=2, arr=result.data)
 
-integration = result.data[0]
 
-order_1 = integration[15:90]
-high_snr = np.where(order_1>40)
-plt.imshow(integration, aspect='auto', vmin=0, vmax=20)
-x = high_snr[1]
-y = high_snr[0]+15
-# plt.plot(x, y, '.', c='y')
+integration = np.median(result.data, axis=0)
 
-deg = 4
-poly_coeff = np.polyfit(x, y, deg)
-poly = np.poly1d(poly_coeff)
-plt.plot(x, poly(x), '.', c='b')
-plt.plot(x, poly(x)+12, '.', c='r')
-plt.plot(x, poly(x)-10, '.', c='r')
+# order_1 = integration[15:90]
+# high_snr = np.where(order_1>40)
+# plt.imshow(integration, aspect='auto', vmin=0, vmax=20)
+# x = high_snr[1]
+# y = high_snr[0]+15
+# # plt.plot(x, y, '.', c='y')
 
-
-mask = np.ones_like(integration, dtype=bool)
-mask[120:, :900] = False
-mask[:90] = False
-mask[:, 1750:] = False
-mask[240:] = False
-mask[80:95, 1300:1750] = False
-
-order_2 = integration[80:250, 650:1800]
-high_snr_2 = np.where((integration>5) & mask)
-x_2 = high_snr_2[1]
-y_2 = high_snr_2[0]
-# plt.plot(x_2, y_2, '.', c='y')
-
-# deg = 5
-# poly_coeff = np.polyfit(x_2, y_2, deg)
+# deg = 4
+# poly_coeff = np.polyfit(x, y, deg)
 # poly = np.poly1d(poly_coeff)
-# plt.plot(x_2, poly(x_2), '.', c='b')
-# plt.plot(x_2, poly(x_2)+14, '.', c='r')
-# plt.plot(x_2, poly(x_2)-14, '.', c='r')
+# plt.plot(x, poly(x), '.', c='b')
+# plt.plot(x, poly(x)+25, '.', c='r')
+# plt.plot(x, poly(x)-25, '.', c='r')
 
 
-gaps = np.diff(x_2)
-gap_min = 10
-gap_start_idx = np.where(gaps > gap_min)[0]
-gap_start_idx = gap_start_idx[0]
+# mask = np.ones_like(integration, dtype=bool)
+# mask[120:, :900] = False
+# mask[:90] = False
+# mask[:, 1750:] = False
+# mask[240:] = False
+# mask[80:95, 1300:1750] = False
 
-x_before_gap = x_2[gap_start_idx]
-x_after_gap = x_2[gap_start_idx + 1]
-y_before_gap = y_2[gap_start_idx]
-y_after_gap = y_2[gap_start_idx + 1]
+# order_2 = integration[80:250, 650:1800]
+# high_snr_2 = np.where((integration>5) & mask)
+# x_2 = high_snr_2[1]
+# y_2 = high_snr_2[0]
+# # plt.plot(x_2, y_2, '.', c='y')
 
-poly = Polynomial.fit([x_before_gap, x_after_gap], [y_before_gap, y_after_gap], deg=1)
-x_gap = np.linspace(x_before_gap, x_after_gap, 2000)
+# gaps = np.diff(x_2)
+# gap_min = 10
+# gap_start_idx = np.where(gaps > gap_min)[0]
+# gap_start_idx = gap_start_idx[0]
 
-x_comb = np.concatenate([x_2[:gap_start_idx + 1], x_gap, x_2[gap_start_idx + 1:]])
-y_comb = np.concatenate([y_2[:gap_start_idx + 1], poly(x_gap), y_2[gap_start_idx + 1:]])
+# x_before_gap = x_2[gap_start_idx]
+# x_after_gap = x_2[gap_start_idx + 1]
+# y_before_gap = y_2[gap_start_idx]
+# y_after_gap = y_2[gap_start_idx + 1]
+
+# poly = Polynomial.fit([x_before_gap, x_after_gap], [y_before_gap, y_after_gap], deg=1)
+# x_gap = np.linspace(x_before_gap, x_after_gap, 2000)
+
+# x_comb = np.concatenate([x_2[:gap_start_idx + 1], x_gap, x_2[gap_start_idx + 1:]])
+# y_comb = np.concatenate([y_2[:gap_start_idx + 1], poly(x_gap), y_2[gap_start_idx + 1:]])
+
+# deg = 4
+# poly_coeff = np.polyfit(x_comb, y_comb, deg)
+# poly = np.poly1d(poly_coeff)
+
+# x_dense = np.linspace(min(x_comb), max(x_comb), 2000)
+# plt.plot(x_comb, poly(x_comb), '.', c='b')
+# plt.plot(x_comb, poly(x_comb)+14, '.', c='r')
+# plt.plot(x_comb, poly(x_comb)-14, '.', c='r')
+
+
+plt.figure('fig1')
+plt.plot(integration[:,340])
+
+plt.figure('fig2')
+plt.plot(np.gradient(integration[:,340]))
+
+box = 5
+bbox = np.ones(box) / box
+a = np.convolve(integration[:,340], bbox, 'same')
+plt.figure('fig1')
+plt.plot(a)
+
+plt.figure('fig2')
+plt.plot(np.gradient(a))
+
+order1_first = []
+order1_last = []
+box = 5
+bbox = np.ones(box) / box
+
+for i in range(4, integration.shape[1]-4):
+    order1 = integration[:95, i]
+    smooth1 = np.convolve(order1, bbox, 'same')
+    der1 = np.gradient(smooth1)
+    max_der1 = np.argmax(der1)
+    min_der1 = np.argmin(der1)
+    order1_first.append(max_der1)
+    order1_last.append(min_der1)
+    
+x_1 = np.arange(4, integration.shape[1]-4)
+
+plt.figure('test')
+plt.imshow(integration, aspect='auto', vmin=0, vmax=20)
+# plt.plot(x_1, order1_first, '.', c='r')
+# plt.plot(x_1, order1_last, '.', c='r')
+
+deg = 5
+poly_coeff = np.polyfit(x_1, order1_first, deg)
+poly = np.poly1d(poly_coeff)
+plt.plot(x_1, poly(x_1)-12, '.', c='b')
+poly_coeff = np.polyfit(x_1, order1_last, deg)
+poly = np.poly1d(poly_coeff)
+plt.plot(x_1, poly(x_1)+10, '.', c='b')
+
+order2_first = []
+order2_last = []
+
+for j in range(4, 1750):
+    if j<700:
+        order2 = integration[100:120, j]
+        smooth2 = np.convolve(order2, bbox, 'same')
+        der2 = np.gradient(smooth2)
+        min_der2 = np.argmin(der2)
+        order2_first.append(min_der2+75)
+        order2_last.append(min_der2+100)
+        
+        
+    else:
+        order2 = integration[75:241, j]
+        smooth2 = np.convolve(order2, bbox, 'same')
+        der2 = np.gradient(smooth2)
+        max_der2 = np.argmax(der2)
+        min_der2 = np.argmin(der2)
+        order2_first.append(max_der2+75)
+        order2_last.append(min_der2+75)
+
+# avg width = 25
+x_2 = np.arange(4, 1750)
+
+# plt.plot(x_2, order2_first, '.', c='r')
+# plt.plot(x_2, order2_last, '.', c='r')
 
 deg = 4
-poly_coeff = np.polyfit(x_comb, y_comb, deg)
+poly_coeff = np.polyfit(x_2, order2_first, deg)
 poly = np.poly1d(poly_coeff)
+plt.plot(x_2, poly(x_2)-4, '.', c='b')
+poly_coeff = np.polyfit(x_2, order2_last, deg)
+poly = np.poly1d(poly_coeff)
+plt.plot(x_2, poly(x_2)+3, '.', c='b')
 
-x_dense = np.linspace(min(x_comb), max(x_comb), 2000)
-plt.plot(x_dense, poly(x_dense), '.', c='b')
-plt.plot(x_dense, poly(x_dense)+14, '.', c='r')
-plt.plot(x_dense, poly(x_dense)-14, '.', c='r')
 
-
-# x_comb = x_comb.reshape(-1, 1)
-# x_train, x_test, y_train, y_test = train_test_split(x_comb, y_comb, test_size=0.2, random_state=42)
+# order1_first = order1_first.reshape(-1, 1)
+# x_train, x_test, y_train, y_test = train_test_split(x, order1_first, test_size=0.2, random_state=42)
 
 # degrees = [1,2,3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 # train_errors = []
@@ -168,4 +242,4 @@ plt.plot(x_dense, poly(x_dense)-14, '.', c='r')
 #     test_errors.append(mean_squared_error(y_test, y_test_pred))
 
 # best_degree = degrees[np.argmin(test_errors)]
-# print(f"The best degree based on the test error is: {best_degree}")
+# print(best_degree)
